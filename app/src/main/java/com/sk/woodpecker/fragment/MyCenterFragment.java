9192086de59c1642e0680eae72804c39fbfa86f8
jiangminglu <jiangminglu@gmail.com>
2015-10-28 15:59:37 +0800

    commit

--- app/src/main/java/com/sk/woodpecker/fragment/MyCenterFragment.java
+++ app/src/main/java/com/sk/woodpecker/fragment/MyCenterFragment.java
@@ -6,6 +6,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.sk.woodpecker.R;
 
@@ -20,11 +23,33 @@ public class MyCenterFragment extends BaseFragment {
     }
 
 
+    private ImageView myAvatarImg;
+    private TextView myNameTv;
+    private TextView myTelTv;
+    private LinearLayout myFavLayout;
+    private LinearLayout myFeedbackLayout;
+    private LinearLayout myPwdLayout;
+    private LinearLayout myHelpLayout;
+    private LinearLayout myServiceLayout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_center, container, false);
+        return inflater.inflate(R.layout.fragment_my_center, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        myAvatarImg = (ImageView) view.findViewById(R.id.my_avatar_img);
+        myNameTv = (TextView) view.findViewById(R.id.my_name_tv);
+        myTelTv = (TextView) view.findViewById(R.id.my_tel_tv);
+        myFavLayout = (LinearLayout) view.findViewById(R.id.my_fav_layout);
+        myFeedbackLayout = (LinearLayout) view.findViewById(R.id.my_feedback_layout);
+        myPwdLayout = (LinearLayout) view.findViewById(R.id.my_pwd_layout);
+        myHelpLayout = (LinearLayout) view.findViewById(R.id.my_help_layout);
+        myServiceLayout = (LinearLayout) view.findViewById(R.id.my_service_layout);
     }
 
 
